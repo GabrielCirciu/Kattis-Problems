5eb5fb6edfaddd768d8c960d781c868dc883b21f
Gabriel Cristian Circiu <circiu_gabriel@outlook.com>
2023-09-22 21:29:47 +0200

    Update UNSOLVED-Almennir-Borgarar.py

--- Unknowns/UNSOLVED-Almennir-Borgarar.py
+++ Unknowns/UNSOLVED-Almennir-Borgarar.py
@@ -1,4 +1,16 @@
 first_line = input().split()
-number_of_grills = first_line[0]
-number_of_contestants = first_line[1]
+number_of_grills = int(first_line[0])
+number_of_contestants = int(first_line[1])
 seconds_needed = input().split()
+
+seconds_passed = 0
+waiting = True
+while waiting:
+    seconds_passed += 1
+    for grill_seconds in seconds_needed:
+        if seconds_passed % int(grill_seconds) == 0:
+            number_of_contestants -= 1
+    if number_of_contestants < 0:
+        waiting = False
+
+print(seconds_passed)
