03a42b2f8c656d2270c55ae8b93d046f82d73fbf
Gabriel Cristian Circiu <circiu_gabriel@outlook.com>
2024-02-01 20:59:10 +0100

    Finished Disjoint-Set
    Personal version instead of Union Find

--- Unknowns/Disjoint-Sets.py
+++ Unknowns/Disjoint-Sets.py
@@ -1,25 +1,31 @@
-# n - number of singletons, m - number of lines of input
 n, m = map(int, input().split())
-sets = dict()
+elements_to_representatives, sets_to_elements = dict(), dict()
 
 # Create disjoint sets {0}{1}{2}...{n-1}
 for i in range(n):
-    sets[i] = i
+    elements_to_representatives[i] = i
+    sets_to_elements[i] = [i]
 
-# Process each row of input
 for _ in range(m):
-    operation, x, y = map(int, input().split())
+    # Perform operations m times
+    operation, s, t = map(int, input().split())
 
-    # Query if two elements are part of the same set
+    # Query, if two elements are part of the same set
     if operation == 0:
-        print("1") if sets[x] == sets[y] else print("0")
+        print("1") if elements_to_representatives[s] == elements_to_representatives[t] else print("0")
 
-    # Union two sets based on which element x and y is in which set
+    # Union, merge two sets based on which set x and y is in
     elif operation == 1:
-        if sets[x] != sets[y]:
-            pass
+        if elements_to_representatives[s] != elements_to_representatives[t]:
+            for element in sets_to_elements[elements_to_representatives[t]]:
+                elements_to_representatives[element] = elements_to_representatives[s]
+                sets_to_elements[elements_to_representatives[s]].append(element)
+            #del sets_to_elements[t] - Not needed and gives runtime error anyways
 
-    # Move element from one set to another
+    # Move element s from its set to set containing t
     else:
-        if sets[x] != sets[y]:
-            sets[y] = sets[x]
+        if elements_to_representatives[s] != elements_to_representatives[t]:
+            sets_to_elements[elements_to_representatives[t]].append(s)
+            sets_to_elements[elements_to_representatives[s]].remove(s)
+            elements_to_representatives[s] = elements_to_representatives[t]
+            pass
