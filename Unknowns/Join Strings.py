fd2c8b35b5688506a11689578a55d4b9d25829ce
Gabriel Cristian Circiu <gaci@itu.dk>
2024-02-27 12:07:44 +0100

    Update Join Strings.py

--- Unknowns/Join Strings.py
+++ Unknowns/Join Strings.py
@@ -8,8 +8,13 @@ for i in range(N):
 if N > 1:
     for _ in range(N-1):
         a, b = map(int, input().split())
-        a, b = a - 1, b - 1
-        words[a] = words[a] + words[b]
-        words[b] = ''
+        words[a - 1] += words[b - 1]
+        words[b - 1] = ''
 
-print(words[a])
+print(words[a - 1])
+
+# Design it with a dict with pointers
+# Each key will be the position in the "list"
+# Values will be the word and the pointer to the next element in the list
+# Initially all keys point to themselves
+# Pointers get updated with commands, similar to a mix of linked list with a graph
